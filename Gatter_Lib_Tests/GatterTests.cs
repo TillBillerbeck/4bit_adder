8bfda4e8f37dc5460a7cfc05289848a7fe994847
Till Billerbeck <tillbillerbeck@protonmail.com>
2020-03-09 22:45:03 +0100

    Add files via upload

---
+++ Gatter_Lib_Tests/GatterTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GatterLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GatterLib.Tests
+{
+    [TestClass()]
+    public class GatterTests
+    {
+        [TestMethod()]
+        public void eingebenTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
