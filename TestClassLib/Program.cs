db112002fc8182d36cf1b8cfccd86be813e8a0fb
Till Billerbeck <tillbillerbeck@protonmail.com>
2020-03-09 22:45:31 +0100

    Add files via upload

---
+++ TestClassLib/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gatter;
+
+namespace TestClassLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NotGatter not = new NotGatter();
+            not.eingeben(false);
+            not.berechnen();
+            Console.WriteLine(not.ausgebenSF());
+
+            AndGatter and = new AndGatter();
+            and.eingeben(false);
+            and.berechnen();
+            Console.WriteLine(and.ausgebenSF());
+
+            NandGatter nand = new NandGatter();
+            nand.eingeben(false);
+            nand.berechnen();
+            Console.WriteLine(nand.ausgebenSF());
+
+            Console.WriteLine(NandGatter.Berechnen(false, true));
+
+            Console.ReadLine();
+        }
+    }
+}
